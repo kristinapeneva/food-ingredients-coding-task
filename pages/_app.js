0fef749c928d1f63672247f3f9b829a6d1ef9bef
Kristina K. Peneva <85350642+kristinapeneva@users.noreply.github.com>
2022-10-12 22:42:13 +0200

    api fixed, basic form added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
+import Head from 'next/head'
+import { ChakraProvider } from '@chakra-ui/react'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        Food ingreadients search
+        <link rel='icon' href="/favicon.ico" />
+      </Head>
+    
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
+        
+    </>
+  )
 }
 
 export default MyApp
